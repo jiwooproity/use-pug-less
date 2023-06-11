dae736c3ae3379952db3a3a2920567124d71ac32
jiwooproity <jiwooproity@naver.com>
2023-06-11 21:43:54 +0900

    practice: less-middleware를 통한 less 스타일시트 css 컴파일, 화면 출력
    pug 화면과 정적 디렉토리를 제공하고 less-middleware로 less 파일을 css
    파일로 컴파일하여 화면을 출력한다

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
 const path = require("path");
+// less 스타일시트 언어 컴파일
+const lessMiddleware = require("less-middleware");
 
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// View Engine으로 Pug 사용 및 Pug 디렉토리 제공
 app.set("views", path.resolve(__dirname, "./views"));
-app.set("views", path.resolve(__dirname, "./test"));
 app.set("view engine", "pug");
 
-app.get("/artist", (req, res) => {
-  const { group } = req.query;
-  res.render("group", { group });
-});
+// 정적 디렉토리 제공
+app.use(lessMiddleware(path.resolve(__dirname, "./styles")));
+app.use(express.static(path.resolve(__dirname, "./styles")));
 
-app.get("/test/:id", (req, res) => {
-  const { id } = req.params;
-  res.render("test", { id });
+app.get("/artist", (req, res) => {
+  let { group, description, songs } = req.query;
+  songs = songs.split(",");
+  res.render("group", { group, description, songs });
 });
 
 app.get("/:nickname", (req, res) => {
@@ -23,6 +25,4 @@ app.get("/:nickname", (req, res) => {
   res.render("index", { nickname });
 });
 
-app.listen(port, () => {
-  console.log(`online to ${port}`);
-});
+app.listen(port);
